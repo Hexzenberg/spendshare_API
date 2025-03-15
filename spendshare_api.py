cfbce3f0926586c0f959dda06618924f5069972b
Nikhil Kumar Chaudhary <nkc150802@gmail.com>
2025-03-15 21:07:36 +0530

    Update spendshare_api.py

--- spendshare_api.py
+++ spendshare_api.py
@@ -42,6 +42,5 @@ def predict():
         return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
-    # Render provides a dynamic PORT, so use os.environ.get("PORT")
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
